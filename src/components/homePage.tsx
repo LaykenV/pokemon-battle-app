d581796e7037712beadf477ed3aa764227c467aa
LaykenV <laykenv@gmail.com>
2022-06-17 11:48:34 -0500

    some structure to home page

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -1,13 +1,44 @@
 import React from "react";
+import "./homePage.css";
 
 function HomePage() {
     return (
         <div className="homeDiv">
-            <div className="homeHeader"></div>
+            <div className="homeHeader">
+                <div className="homeTitle">Pokemon Battle</div>
+                <div className="trainersAndDescription">
+                    <div className="trainerLeft">
+                        <img src="" alt="" className="trainerPic"></img>
+                        <div className="trainerPokemon">
+                            <img src="" alt="" className="battlePokemon"></img>
+                            <img src="" alt="" className="battlePokemon"></img>
+                            <img src="" alt="" className="battlePokemon"></img>
+                        </div>
+                    </div>
+                    <div className="battleDescription">Choose your pokemon and get ready to battle!</div>
+                    <div className="trainerRight">
+                        <img src="" alt="" className="trainerPic"></img>
+                        <div className="trainerPokemon">
+                            <img src="" alt="" className="battlePokemon"></img>
+                            <img src="" alt="" className="battlePokemon"></img>
+                            <img src="" alt="" className="battlePokemon"></img>
+                        </div>
+                    </div>
+                </div>
+                <div className="blackBar">
+                    <div className="bigCircle">
+                        <div className="lilCircle"></div>
+                    </div>
+                </div>
+            </div>
             <div className="homeBody">
-                <div className="trainerLeft"></div>
-                <div className="bodyMain"></div>
-                <div className="trainerRight"></div>
+                <div className="bodyMain">
+                    <div className="pokemonCard"></div>
+                    <div className="pokemonCard"></div>
+                    <div className="pokemonCard"></div>
+                    <div className="pokemonCard"></div>
+                    <div className="pokemonCard"></div>
+                </div>
             </div>
         </div>
     )
