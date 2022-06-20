7ebefcbfce54fbf9b616ecb499b211fa0ec9ad8c
LaykenV <laykenv@gmail.com>
2022-06-20 14:02:09 -0500

    added images, added pokemon api fetch, added cards to home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,78 @@ import './App.css';
 import BattlePage from './components/battlePage';
 import HomePage from './components/homePage';
 
-
+type pokemonArray = {
+  id: number;
+  name: string;
+  frontImage: string;
+  backImage: string;
+  hp: number;
+  attack: number;
+  defense: number;
+  specialAttack: number;
+  specialDefense: number;
+  speed: number;
+  type: string;
+}
 function App() {
+  const [pokemons, setPokemons] = useState([] as pokemonArray[]);
+  const [yourPokemons, setYourPokemons] = useState([] as any);
+  const [a, setA] = useState(1);
+  const [b, setB] = useState(10);
+
+
+  const fetchPokemons = async (a:number, b:number) => {
+    const pokemons: pokemonArray[] = [];
+    for (let i = a; i <= b; i++) {
+      const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${i}`
+      const response = await fetch(pokemonUrl)
+      const pokemon = await response.json()
+      const id : number = pokemon.id;
+      const name = capitalizeFirstLetter(pokemon.name)
+      const frontImage: string = pokemon.sprites.front_default;
+      const backImage : string = pokemon.sprites.back_default;
+      const hp : number = pokemon.stats[0].base_stat;
+      const attack : number = pokemon.stats[1].base_stat;
+      const defense : number = pokemon.stats[2].base_stat;
+      const specialAttack : number = pokemon.stats[3].base_stat;
+      const specialDefense : number = pokemon.stats[4].base_stat;
+      const speed : number = pokemon.stats[5].base_stat;
+      const type : string = pokemon.types[0].type.name;
+      pokemons.push({ id, name, frontImage, backImage, hp, attack, defense, specialAttack, specialDefense, speed, type })
+    }
+    setPokemons(pokemons);
+    console.log(pokemons);
+  }
+
+  useEffect(() => {
+    fetchPokemons(a,b);    
+  }, []);
+
+  useEffect(() => {
+    fetchPokemons(a,b);    
+  }, [b]);
+
+  const capitalizeFirstLetter = (string:string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const handleNextArrow = () => {
+    setA(a + 10);
+    setB(b + 10);    
+  }
+
+  const handleLastArrow = () => {
+    if (a > 1 && b > 10) {
+      setA(a - 10);
+      setB(b - 10)
+    }    
+  }
+
+
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage pokemons={pokemons} handleNextArrow={handleNextArrow} handleLastArrow={handleLastArrow}/>} />
         <Route path="/battle" element={<BattlePage />} />
       </Routes>
     </BrowserRouter>
--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./homePage.css";
+import ball from "./images/pokemonball.png";
+import trainerRed from "./images/trainerRed.png";
+import trainerBlue from "./images/trainerBlue.png";
+import { AnyRecord } from 'dns';
 
-function HomePage() {
+type pokemonArray = {
+    id: number;
+    name: string;
+    frontImage: string;
+    backImage: string;
+    hp: number;
+    attack: number;
+    defense: number;
+    specialAttack: number;
+    specialDefense: number;
+    speed: number;
+    type: string;
+  }
+
+  type homePageProps = {
+      pokemons: pokemonArray[];
+      handleNextArrow: any;
+      handleLastArrow: any;
+  }
+
+
+const HomePage: React.FunctionComponent<homePageProps> = ({pokemons, handleNextArrow, handleLastArrow}) => {
+
+
     return (
         <div className="homeDiv">
             <div className="homeHeader">
-                <div className="homeTitle">Pokemon Battle</div>
+                <div className="homeTitle" onClick={handleNextArrow}>Pokemon Battle</div>
                 <div className="trainersAndDescription">
                     <div className="trainerLeft">
-                        <img src="" alt="" className="trainerPic"></img>
+                        <img src={trainerRed} alt="trainer" className="trainerPic"></img>
                         <div className="trainerPokemon">
-                            <img src="" alt="" className="battlePokemon"></img>
-                            <img src="" alt="" className="battlePokemon"></img>
-                            <img src="" alt="" className="battlePokemon"></img>
+                            <img src={ball} alt="ball" className="battlePokemon"></img>
+                            <img src={ball} alt="ball" className="battlePokemon"></img>
+                            <img src={ball} alt="ball" className="battlePokemon"></img>
                         </div>
                     </div>
-                    <div className="battleDescription">Choose your pokemon and get ready to battle!</div>
+                    <div className="battleDescription" onClick={handleLastArrow}>Choose your pokemon and get ready to battle!</div>
                     <div className="trainerRight">
-                        <img src="" alt="" className="trainerPic"></img>
+                        <img src={trainerBlue} alt="trainer" className="trainerPic"></img>
                         <div className="trainerPokemon">
-                            <img src="" alt="" className="battlePokemon"></img>
-                            <img src="" alt="" className="battlePokemon"></img>
-                            <img src="" alt="" className="battlePokemon"></img>
+                            <img src={ball} alt="ball" className="battlePokemon"></img>
+                            <img src={ball} alt="ball" className="battlePokemon"></img>
+                            <img src={ball} alt="ball" className="battlePokemon"></img>
                         </div>
                     </div>
                 </div>
@@ -32,13 +59,30 @@ function HomePage() {
                 </div>
             </div>
             <div className="homeBody">
+                <div className='leftArrow'>Last Page</div>
                 <div className="bodyMain">
-                    <div className="pokemonCard"></div>
-                    <div className="pokemonCard"></div>
-                    <div className="pokemonCard"></div>
-                    <div className="pokemonCard"></div>
-                    <div className="pokemonCard"></div>
+                    {pokemons.map((pokemon) => {
+                    return (
+                        <div className='cardContainer' key={pokemon.id}>
+                            <div className='card'>
+                                <img src={pokemon.frontImage} alt={pokemon.name}></img>
+                                <div className='pokemonName'>{pokemon.name}</div>
+                            </div>
+                            <div className='stats'>
+                                <div className='pokemonType'>Type: {pokemon.type}</div>
+                                <div className='pokemonStat'>HP: {pokemon.hp}</div>
+                                <div className='pokemonStat'>Attack: {pokemon.attack}</div>
+                                <div className='pokemonStat'>Defense: {pokemon.defense}</div>
+                                <div className='pokemonStat'>Special Attack: {pokemon.specialAttack}</div>
+                                <div className='pokemonStat'>Special Defense: {pokemon.specialDefense}</div>
+                                <div className='pokemonStat'>Speed: {pokemon.speed}</div>
+                                <button className='addToTeam'>Add to team</button>
+                            </div>
+                        </div>
+                    )
+                    })}
                 </div>
+                <div className='rightArrow'>Next Page</div>
             </div>
         </div>
     )
